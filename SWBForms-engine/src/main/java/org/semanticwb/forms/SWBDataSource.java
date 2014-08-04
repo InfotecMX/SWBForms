73eca669890e2061b892600f6d255562f07c9c0b
javiersolis <javiersolis@192.168.5.116>
2014-08-04 15:53:29 -0500

    Soporte a DataProcessors y DataStores

--- SWBForms-engine/src/main/java/org/semanticwb/forms/SWBDataSource.java
+++ SWBForms-engine/src/main/java/org/semanticwb/forms/SWBDataSource.java
@@ -8,9 +8,10 @@ import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.util.JSON;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.util.Iterator;
 import java.util.Map;
-import org.semanticwb.forms.datasource.DataSourceMongo;
+import org.semanticwb.forms.datastore.DataStore;
 import org.semanticwb.forms.script.ScriptObject;
 
 /**
@@ -19,19 +20,43 @@ import org.semanticwb.forms.script.ScriptObject;
  */
 public class SWBDataSource 
 {
+    public static final String ACTION_FETCH="fetch";
+    public static final String ACTION_UPDATE="update";
+    public static final String ACTION_ADD="add";
+    public static final String ACTION_REMOVE="remove";
+    public static final String ACTION_VALIDATE="validate";
     
     private String name=null;
     private SWBScriptEngine engine=null;
     private ScriptObject script=null;
-    private DataSourceMongo db=null;
+    private DataStore db=null;
     
-    protected SWBDataSource(String name, ScriptObject script, SWBScriptEngine engine)
+    protected SWBDataSource(String name, SWBScriptEngine engine)
     {
+        System.out.println("SWBDataSource:"+name);
         this.name=name;
         this.engine=engine;
-        this.script=script;
+//        this.script=script;
         
-        db=new DataSourceMongo(this);
+        ScriptObject swbf=new ScriptObject(engine.getNativeScriptEngine().get("swbf"));
+        ScriptObject dataSources=swbf.get("dataSources");   
+        this.script=dataSources.get(name);
+
+        if(this.script==null)throw new NoSuchFieldError("DataSource not found:"+name);
+        
+        String dataStoreName=this.script.getString("dataStore");
+        ScriptObject dataStore=swbf.get("dataStores").get(dataStoreName);
+        
+        String dataStoreClass=dataStore.getString("class");
+        
+        System.out.println(dataStoreName+":"+dataStoreClass);
+        
+        try
+        {
+            Class cls=Class.forName(dataStoreClass);
+            Constructor c=cls.getConstructor(ScriptObject.class, SWBDataSource.class);
+            db=(DataStore)c.newInstance(dataStore,this);
+        }catch(Exception e){e.printStackTrace();}        
     }
 
     /**
@@ -71,8 +96,70 @@ public class SWBDataSource
     
     public BasicDBObject fetch(BasicDBObject json) throws IOException
     {
-        return db.fetch(json);
+        BasicDBObject req=engine.invokeDataProcessors(name, SWBDataSource.ACTION_FETCH, SWBDataProcessor.METHOD_REQUEST, json);
+        BasicDBObject res=db.fetch(req);
+        res=engine.invokeDataProcessors(name, SWBDataSource.ACTION_FETCH, SWBDataProcessor.METHOD_RESPONSE, res);
+        engine.invokeDataServices(name, SWBDataSource.ACTION_FETCH, req, res);
+        return res;
     }
+
+    public BasicDBObject addObj(BasicDBObject obj) throws IOException
+    {
+        BasicDBObject ret=null;
+        BasicDBObject req=new BasicDBObject();
+        req.append("data", obj);        
+        ret=add(req);
+        return ret;
+    }    
+    
+    public BasicDBObject updateObj(BasicDBObject obj) throws IOException
+    {
+        BasicDBObject ret=null;
+        BasicDBObject req=new BasicDBObject();
+        req.append("data", obj);        
+        ret=update(req);
+        return ret;
+    }
+    
+    public BasicDBObject fetchObjById(String id) throws IOException
+    {
+        BasicDBObject ret=null;
+        BasicDBObject req=new BasicDBObject();
+        BasicDBObject data=new BasicDBObject();
+        data.append("_id", id);
+        req.put("data", data);
+
+        BasicDBObject r=(BasicDBObject)fetch(req);
+        if(r!=null)
+        {
+            BasicDBObject res=(BasicDBObject)r.get("response");       
+            if(res!=null)
+            {
+                BasicDBList rdata=(BasicDBList)res.get("data");
+                if(rdata!=null && rdata.size()>0)
+                {
+                    ret=(BasicDBObject)rdata.get(0);
+                }
+            }            
+        }
+        return ret;
+    }
+    
+    public BasicDBObject removeObjById(String id) throws IOException
+    {
+        BasicDBObject ret=null;
+        BasicDBObject req=new BasicDBObject();
+        BasicDBObject data=new BasicDBObject();
+        data.append("_id", id);
+        req.put("data", data);
+
+        BasicDBObject r=(BasicDBObject)remove(req);
+        if(r!=null)
+        {
+            ret=(BasicDBObject)r.get("response");       
+        }
+        return ret;
+    }   
     
     public BasicDBObject update(String query) throws IOException
     {
@@ -81,7 +168,11 @@ public class SWBDataSource
     
     public BasicDBObject update(BasicDBObject json) throws IOException
     {
-        return db.update(json);
+        BasicDBObject req=engine.invokeDataProcessors(name, SWBDataSource.ACTION_UPDATE, SWBDataProcessor.METHOD_REQUEST, json);
+        BasicDBObject res=db.update(req);
+        res=engine.invokeDataProcessors(name, SWBDataSource.ACTION_UPDATE, SWBDataProcessor.METHOD_RESPONSE, res);
+        engine.invokeDataServices(name, SWBDataSource.ACTION_UPDATE, req, res);
+        return res;
     }    
     
     public BasicDBObject add(String query) throws IOException
@@ -91,7 +182,11 @@ public class SWBDataSource
     
     public BasicDBObject add(BasicDBObject json) throws IOException
     {
-        return db.add(json);
+        BasicDBObject req=engine.invokeDataProcessors(name, SWBDataSource.ACTION_ADD, SWBDataProcessor.METHOD_REQUEST, json);
+        BasicDBObject res=db.add(req);
+        res=engine.invokeDataProcessors(name, SWBDataSource.ACTION_ADD, SWBDataProcessor.METHOD_RESPONSE, res);
+        engine.invokeDataServices(name, SWBDataSource.ACTION_ADD, req, res);
+        return res;
     }    
     
     public BasicDBObject remove(String query) throws IOException
@@ -101,7 +196,11 @@ public class SWBDataSource
     
     public BasicDBObject remove(BasicDBObject json) throws IOException
     {
-        return db.remove(json);
+        BasicDBObject req=engine.invokeDataProcessors(name, SWBDataSource.ACTION_REMOVE, SWBDataProcessor.METHOD_REQUEST, json);
+        BasicDBObject res=db.remove(req);
+        res=engine.invokeDataProcessors(name, SWBDataSource.ACTION_REMOVE, SWBDataProcessor.METHOD_RESPONSE, res);
+        engine.invokeDataServices(name, SWBDataSource.ACTION_REMOVE, req, res);
+        return res;
     }    
     
     public BasicDBObject validate(String query) throws IOException
@@ -202,27 +301,13 @@ public class SWBDataSource
     public ScriptObject getDataSourceScriptField(String name)
     {
         ScriptObject fields=script.get("fields");
-        if(fields!=null)
+        ScriptObject ret=SWBFormsUtils.getArrayNode(fields, "name", name);
+        if(ret==null)
         {
-            Iterator<ScriptObject> it=fields.values().iterator();
-            while (it.hasNext()) {
-                ScriptObject field = it.next();
-                String fname=field.getString("name");
-                if(fname!=null && fname.equals(name))return field;
-            }
+            fields=script.get("links");
+            ret=SWBFormsUtils.getArrayNode(fields, "name", name);
         }
-        
-        fields=script.get("links");
-        if(fields!=null)
-        {
-            Iterator<ScriptObject> it=fields.values().iterator();
-            while (it.hasNext()) {
-                ScriptObject field = it.next();
-                String fname=field.getString("name");
-                if(fname!=null && fname.equals(name))return field;
-            }
-        }
-        return null;
+        return ret;
     }
             
 //******************************************* static *******************************/            
@@ -237,22 +322,10 @@ public class SWBDataSource
         return ret;
     }
     
-    private static ScriptObject getServerValidator(ScriptObject field, String type)
-    {
-        ScriptObject validators=field.get("validators");
-        if(validators!=null)
-        {
-            Iterator<ScriptObject> it2=validators.values().iterator();
-            while (it2.hasNext()) {
-                ScriptObject validator = it2.next();
-                String stype=validator.getString("type");
-                if(stype!=null && stype.equals(type))
-                {
-                    return validator;
-                }
-            }
-        }
-        return null;        
-    }    
+//    private static ScriptObject getServerValidator(ScriptObject field, String type)
+//    {
+//        ScriptObject validators=field.get("validators");
+//        return SWBFormsUtils.getArrayNode(validators, "type", type);
+//    }    
     
 }
--- SWBForms-engine/src/main/java/org/semanticwb/forms/SWBFormsUtils.java
+++ SWBForms-engine/src/main/java/org/semanticwb/forms/SWBFormsUtils.java
@@ -7,44 +7,47 @@ package org.semanticwb.forms;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
+import org.semanticwb.forms.script.ScriptObject;
 
 /**
  *
  * @author javier.solis.g
  */
-public class SWBFormsUtils
-{
+public class SWBFormsUtils {
+
     /**
      * Lee el contenido del InputStream y lo convierte a un String
+     *
      * @param inputStream
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public static String readInputStream(InputStream inputStream) throws IOException 
-    {
+    public static String readInputStream(InputStream inputStream) throws IOException {
         return new String(readFully(inputStream));
-    }      
-    
+    }
+
     /**
-     * Lee el contenido del InputStream y lo convierte a un String, con la codificacion especificada
+     * Lee el contenido del InputStream y lo convierte a un String, con la
+     * codificacion especificada
+     *
      * @param inputStream
      * @param encoding
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public static String readInputStream(InputStream inputStream, String encoding) throws IOException 
-    {
+    public static String readInputStream(InputStream inputStream, String encoding) throws IOException {
         return new String(readFully(inputStream), encoding);
-    }    
+    }
 
     /**
      * Lee el contenido del InputStream y lo convierte a un arreglo de bytes
+     *
      * @param inputStream
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public static byte[] readFully(InputStream inputStream) throws IOException 
-    {
+    public static byte[] readFully(InputStream inputStream) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int length = 0;
@@ -52,5 +55,22 @@ public class SWBFormsUtils
             baos.write(buffer, 0, length);
         }
         return baos.toByteArray();
-    }      
+    }
+
+    public static ScriptObject getArrayNode(ScriptObject arr, String prop, String value) 
+    {
+        if (arr != null) {
+            Iterator<ScriptObject> it1 = arr.values().iterator();
+            while (it1.hasNext()) {
+                ScriptObject obj = it1.next();
+                String val = obj.getString(prop);
+                if (val != null && val.equals(value)) 
+                {
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
 }
